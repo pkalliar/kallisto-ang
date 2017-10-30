61ce76696bf3acc62ac5d4563b1606fa1cab1882
Panagiotis Kalliaras <pkalliar@yahoo.com>
2017-10-30 17:51:08 +0200

    from work

---
+++ src/app/protocols/protocols.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Protocol } from '../protocol';
+import { ProtocolDetailComponent } from '../protocol-detail.component';
+import { ProtocolService } from '../protocols/protocol.service';
+
+// my comment on Monday morning
+@Component({
+  selector: 'protocols',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ProtocolService]
+})
+export class ProtocolsComponent implements OnInit {
+  title = 'Angular pankal app';
+  selectedProtocol: Protocol;
+
+  protocols: Protocol[];
+
+  constructor(private protocolService: ProtocolService) { }
+
+  getProtocols(): void {
+    this.protocolService.getProtocolsSlowly().then(protocols => this.protocols = protocols);
+  }
+
+  ngOnInit(): void {
+    this.getProtocols();
+  }
+
+  onSelect(protocol: Protocol): void {
+    this.selectedProtocol = protocol;
+  }
+
+}
+
+
+
