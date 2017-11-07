7844194ea5b03f613fa1b995b8933d55f7fb8130
Panagiotis Kalliaras <pkalliar@yahoo.com>
2017-11-07 16:22:55 +0200

    douleia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,44 @@ export class AppComponent implements OnInit {
   }
 
 
+  countdownToLogout = function(){
+
+      var interval = 1000;
+      
+      // moment.locale('el');
+
+      // var datePattern = "YYYY-MM-DD HH:mm:ss Z";
+
+      // $scope.authExpiration = moment(localStorageService.get('apikey_expires'));
+      // $scope.previousPollTime = moment();
+
+      // $interval(function(){
+      //   $scope.authExpiration = moment(localStorageService.get('apikey_expires'));
+      //   var pollInnterval = 5;  
+
+      //   var b = moment();
+      //   var diffSec = $scope.authExpiration.diff(b, 'seconds');
+      //   var diffMin = $scope.authExpiration.diff(b, 'minutes');
+      //   var diffSec2 = diffSec - diffMin*60;
+      //   if (diffSec2 < 10) {diffSec2 = "0"+diffSec2;}
+
+      //   if(diffSec >= 45 && diffSec > 0){
+      //       $scope.countdown = diffMin + ":" + diffSec2;
+      //   }else if(diffSec <= 0 && diffSec < -2){
+      //       console.log("Αποσυνδέεστε.." );
+      //       $scope.countdown = 'Αποσυνδέεστε..1';
+      //       $scope.logout();
+      //   }else{
+      //       console.log("Αποσυνδέεστε..2" );
+      //       // $scope.logout();
+      //   }
+      //   var diffPollingSec = b.diff($scope.previousPollTime, 'seconds');
+      //   if(diffPollingSec >= pollInnterval){
+      //     //$scope.getServerEvents();
+      //   }
+        
+      // }, interval);
+    }
 
 }
 
