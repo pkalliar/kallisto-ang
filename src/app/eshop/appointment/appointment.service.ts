bf5457d790136fb404600d9a3943808fb35c23fd
Panagiotis Kalliaras <pkalliar@yahoo.com>
2018-06-26 00:40:31 +0300

    calendar home

--- src/app/eshop/appointment/appointment.service.ts
+++ src/app/eshop/appointment/appointment.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  search_firestore(keyword) {
+    console.log('in search_firestore..');
+
+    return this.afs.firestore.collection('appointments').get().then(querySnapshot => querySnapshot.docs
+    );
+  }
+
 }
--- src/app/eshop/appointment/appointments/appointments.component.ts
+++ src/app/eshop/appointment/appointments/appointments.component.ts
@@ -3,6 +3,7 @@ import { Appointment } from '../appointment';
 import { TableDatabase, TableDataSource } from '../../../utilities';
 import { MatSort } from '@angular/material';
 import { AppointmentService } from '../appointment.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-appointments',
@@ -14,6 +15,9 @@ export class AppointmentsComponent implements OnInit {
   selected: Appointment;
 
   entlist: Appointment[];
+  aps: Array<Appointment> = [];
+
+  searchTerm: FormControl = new FormControl();
 
   currentFilter = '';
   displayedColumns = ['name', 'start_time', 'end_time'];
@@ -26,6 +30,23 @@ export class AppointmentsComponent implements OnInit {
   constructor(private service: AppointmentService) { }
 
   ngOnInit() {
+    this.service.search_firestore(this.searchTerm.value).then(response => {
+      response.forEach((doc) => {
+        const a = new Appointment();
+
+        a.id = doc.id;
+        a.name = doc.get('name');
+        a.start_time = doc.get('start_time');
+        a.end_time = doc.get('end_time');
+
+        this.aps.push(a);
+        this.tableDatabase.addLine(a);
+
+        console.log(doc.get('body'));
+            console.log(`${doc.id} => ${JSON.stringify(doc.data)} `);
+      });
+    });
+    this.dataSource = new TableDataSource(this.tableDatabase, this.sort);
   }
 
 }
